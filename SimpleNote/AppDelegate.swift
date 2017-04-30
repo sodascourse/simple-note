c12198367ab681989d7084159967747c806a7f09
sodastsai <sodas2002@gmail.com>
2017-04-30 18:22:45 +0800

    Init Xcode project

---
+++ SimpleNote/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  SimpleNote
+//
+//  Created by sodas on 4/30/17.
+//  Copyright © 2017 sodastsai. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
